4e2b2f65a4d6d1571e173f2ab6426f2c821c2e84
Rab-son <rabsonsayendajnr@gmail.com>
2023-02-21 01:44:46 +0200

    registration logic

---
+++ harvesthub/app/ussd/components/menu.php
@@ -0,0 +1,41 @@
+<?php 
+
+class Menu{
+
+    protected $text;
+    protected $sessionId;
+    
+    function __construct($text, $sessionId){
+        $this->text = $text;
+        $this->sessionId = $sessionId;
+    }
+
+    public function mainMenuRegistered(){
+        $response = "CON Welcome " .$name. " Reply with \n";
+        $response .="1. Buy Products\n";
+        $response .="2. View Orders\n";
+        $response .="3. Sell Products\n";
+        $response .="4. My Account\n";
+        $response .="5. Help\n";
+        echo $response;
+
+    }
+
+    public function mainMenuUnRegistered(){
+        $response = "CON Welcome HarvestHub\n";
+        $response .="1. Register\n";
+        echo $response;
+    }
+
+    public function registerMenu(){}
+
+    public function viewProductMenu(){}
+
+    public function viewOrdersMenu(){}
+
+    public function submitProductMenu(){}
+
+}
+
+
+?>
--- harvesthub/app/ussd/index.php
+++ harvesthub/app/ussd/index.php
@@ -1,5 +1,7 @@
 <?php 
 
+include_once "./components/menu.php";
+
 //API Variables 
 $sessionId = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
@@ -8,15 +10,17 @@ $text = $_POST["text"];
 
 
 $isRegistered = false;
+$menu = new Menu($text, $sessionId);
 
 
 if($text == "" && $isRegistered){
     // registered
-
+    $menu->mainMenuRegistered();
 
 }else if($text == "" && $isRegistered){
     // unregistered and empty string
-
+    $menu->mainMenuUnRegistered();
+    
 }else if($isRegistered){
     // unregistered and non-empty string
 }else{
