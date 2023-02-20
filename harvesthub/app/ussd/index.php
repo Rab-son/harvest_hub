42f2cf9e1b1c441f9cd843edcb243e4afbfd523b
Rab-son <rabsonsayendajnr@gmail.com>
2023-02-21 01:22:52 +0200

    ussd entry point

---
+++ harvesthub/app/ussd/index.php
@@ -0,0 +1,25 @@
+<?php 
+
+//API Variables 
+$sessionId = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text = $_POST["text"];
+
+
+$isRegistered = false;
+
+
+if($text == "" && $isRegistered){
+    // registered
+
+
+}else if($text == "" && $isRegistered){
+    // unregistered and empty string
+
+}else if($isRegistered){
+    // unregistered and non-empty string
+}
+
+
+?>
