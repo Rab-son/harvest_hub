bee4388fa89ead26a222989a308e012e3025a2cb
Rab-son <rabsonsayendajnr@gmail.com>
2023-02-21 02:05:33 +0200

    util

---
+++ harvesthub/app/ussd/components/util.php
@@ -0,0 +1,22 @@
+<?php 
+    class Util{
+        //DB Variables
+        static $DB_NAME = "harvest_hub";
+        static $DB_USER = "root";
+        static $DB_USER_PASS = "";
+        static $SERVER_NAME ="localhost";
+
+        //About USSD Menu
+        static $GO_BACK = "98";
+        static $GO_TO_MAIN_MENU = "99";
+
+        static $API_KEY ="";
+        static $API_USERNAME = "sandbox";
+
+        static $SMS_SHORTCODE = "623452";
+        static $COMPANY_NAME = "Harvest Hub";
+
+
+    }
+
+?>
