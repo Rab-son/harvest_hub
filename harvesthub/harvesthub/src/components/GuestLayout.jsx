b9f545d9970f0e408009c0c7324989e3a9966505
Rab-son <rabsonsayendajnr@gmail.com>
2023-03-01 08:38:03 +0200

    guest layout

--- harvesthub/harvesthub/src/components/GuestLayout.jsx
+++ harvesthub/harvesthub/src/components/GuestLayout.jsx
@@ -1,58 +1,16 @@
-import {Link, Navigate, Outlet} from "react-router-dom";
-import {useStateContext} from "../context/ContextProvider";
-import axiosClient from "../axios-client.js";
-import {useEffect} from "react";
+import {Navigate, Outlet} from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider";
 
-export default function DefaultLayout() {
-  const {user, token, setUser, setToken, notification} = useStateContext();
+export default function GuestLayout() {
+  const { user, token } = useStateContext();
 
-  if (!token) {
-    return <Navigate to="/login"/>
+  if (token) {
+    return <Navigate to="/" />;
   }
-
-  const onLogout = ev => {
-    ev.preventDefault()
-
-    axiosClient.post('/logout')
-      .then(() => {
-        setUser({})
-        setToken(null)
-      })
-  }
-
-  useEffect(() => {
-    axiosClient.get('/user')
-      .then(({data}) => {
-         setUser(data)
-      })
-  }, [])
 
   return (
-    <div id="defaultLayout">
-      <aside>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/users">Users</Link>
-      </aside>
-      <div className="content">
-        <header>
-          <div>
-            Header
-          </div>
-
-          <div>
-            {user.name} &nbsp; &nbsp;
-            <a onClick={onLogout} className="btn-logout" href="#">Logout</a>
-          </div>
-        </header>
-        <main>
-          <Outlet/>
-        </main>
-        {notification &&
-          <div className="notification">
-            {notification}
-          </div>
-        }
-      </div>
+    <div id="guestLayout">
+      <Outlet />
     </div>
-  )
+  );
 }
