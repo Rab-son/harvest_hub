f13c3050ad9ebf655cc771f89b94a408cc657e57
Rab-son <rabsonsayendajnr@gmail.com>
2023-03-06 22:36:38 +0200

    making product fields fillable

--- harvesthub/app/Models/Product.php
+++ harvesthub/app/Models/Product.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'unit',
+        'price',
+        'quantity',
+        'status',
+    ];
 }
